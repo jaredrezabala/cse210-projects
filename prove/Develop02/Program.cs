4c64394026d37b5e3229aa42ea0f7562b20e3f60
jaredrezabala <rezabalajared@gmail.com>
2023-05-12 20:36:37 -0500

    final changes

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,62 +1,10 @@
 using System;
-using System.IO;
-
-class PromptJournal
-{
-    private string fileName;
-    private string[] prompts = {"What are three things that you're grateful for today?",
-                                "Describe a recent challenge you faced and how you overcame it.",
-                                "Write about a moment that made you feel proud of yourself.",
-                                "What's something that you've been procrastinating on, and what steps can you take to start working on it?",
-                                "Write about a recent accomplishment that you're proud of."};
-
-    public PromptJournal(string fileName)
-    {
-        this.fileName = fileName;
-    }
-
-    public void WritePrompt()
-    {
-        Random random = new Random();
-        int randomNumber = random.Next(0, 5);
-        string randomPrompt = prompts[randomNumber];
-
-        Console.WriteLine(randomPrompt);
-        string promptAnswer = Console.ReadLine();
-
-        DateTime currentDate = DateTime.Now;
-        string fileContent = $"Date:{currentDate.ToString()}\n - Prompt:{randomPrompt}\n Answer:{promptAnswer}"; 
-
-        File.WriteAllText(fileName, fileContent);
-    }
-
-    public void DisplayPrompt()
-    {
-        string fileContentRead = File.ReadAllText(fileName);
-        Console.WriteLine("\nFile content:");
-        Console.WriteLine(fileContentRead);
-    }
-
-    public void LoadPrompts()
-    {
-        string journal = File.ReadAllText(fileName);
-        Console.WriteLine("\nFile content:");
-        Console.WriteLine(journal);
-    }
-
-    public void SavePrompts(string content)
-    {
-        File.AppendAllText(fileName, content);
-    }
-}
 
 class Program
 {
     static void Main(string[] args)
     {
         string choices = "";
-        PromptJournal promptJournal = new PromptJournal("prompts.txt");
-
         do
         {
             Console.WriteLine("Hello Develop02 World!");
@@ -68,25 +16,55 @@ class Program
             Console.WriteLine("5. Quit");
             Console.WriteLine("What would you like to do?");
             choices = Console.ReadLine();
+            string[] prompts = {"What are three things that you're grateful for today?",
+                                "Describe a recent challenge you faced and how you overcame it.",
+                                "Write about a moment that made you feel proud of yourself.",
+                                "What's something that you've been procrastinating on, and what steps can you take to start working on it?",
+                                "Write about a recent accomplishment that you're proud of."};
+            Random random = new Random();
+            int randomNumber = random.Next(0, 5);
+            string randomPrompt = prompts[randomNumber];
+            string fileName = "prompts.txt";
+            string fileContent = "";
 
             if(choices == "1")
             {
-                promptJournal.WritePrompt();
+                Console.WriteLine(randomPrompt);
+                string promptAnswer = Console.ReadLine();
+
+                DateTime currentDate = DateTime.Now;
+                fileContent = $"Date:{currentDate.ToString()}\n - Prompt:{randomPrompt}\n Answer:{promptAnswer}"; 
+                
+                File.WriteAllText(fileName, fileContent);
+
+                // Aquí llamamos a la clase PromptResponder para imprimir una frase de respuesta
+                PromptResponder.Respond(promptAnswer);
+
             }
             else if(choices == "2")
             {
-                promptJournal.DisplayPrompt();
+                string fileContentRead = File.ReadAllText(fileName);
+                Console.WriteLine("\nFile content:");
+                Console.WriteLine(fileContentRead);
             }
             else if(choices == "3")
             {
-                promptJournal.LoadPrompts();
+                File.AppendAllText("prompts.txt", fileContent);
             }
             else if(choices == "4")
             {
-                Console.WriteLine("Enter content to save:");
-                string content = Console.ReadLine();
-                promptJournal.SavePrompts(content);
+                string journal = File.ReadAllText("prompts.txt");
+                Console.WriteLine("\nFile content:");
+                Console.WriteLine(journal);
             }
         } while (choices != "5");
+    }
+}
+
+public static class PromptResponder
+{
+    public static void Respond(string promptAnswer)
+    {
+        Console.WriteLine($"Thanks for your answer: {promptAnswer}");
     }
 }
