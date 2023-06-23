c950f2057e4e7ef1e98aec77dee5d35327997eb3
jaredrezabala <rezabalajared@gmail.com>
2023-06-22 20:20:13 -0500

    loop

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,9 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
+    while (true)
+    {
         Console.WriteLine("Hello Develop05 World!");
         Goal goal = new Goal();
         goal.DisplayMenu();
+    }
         
     }
 }
