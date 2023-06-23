1037b001964152157cbca8b9d259cf8177fee7e7
jaredrezabala <rezabalajared@gmail.com>
2023-06-22 19:44:54 -0500

    goal class initiated in program cs

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -5,5 +5,8 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop05 World!");
+        Goal goal = new Goal();
+        goal.DisplayMenu();
+        
     }
 }
