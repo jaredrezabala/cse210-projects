9601e8e44f8388cffffdec71bc357054fb39a3c7
jaredrezabala <rezabalajared@gmail.com>
2023-06-23 09:37:14 -0500

    Importing classes from Visual Studio

--- prove/Develop05/Goal.cs
+++ prove/Develop05/Goal.cs
@@ -1,109 +1,44 @@
 using System;
-using System.IO;
-class Goal
+
+public abstract class Goal
 {
-    List<string[]> goalList = new List<string[]>();
+    // Attributes
+    private string _type;
+    private string _name;
+    private string _description;
+    private int _points;
 
-    public void DisplayScore()
+
+    // Constructors
+    public Goal(string type, string name, string description, int points)
     {
-        Console.WriteLine("You have x points");
-        Console.WriteLine();
-
+        _type = type;
+        _name = name;
+        _description = description;
+        _points = points;
     }
-    public void DisplayMenu()
+    public string GetType()
     {
-        bool runProgram = false;
-        while(runProgram != true)
-        {
-            Console.WriteLine("Menu Options:");
-            Console.WriteLine("1) Create New Goal");
-            Console.WriteLine("2) List Goals");
-            Console.WriteLine("3) Save Goals");
-            Console.WriteLine("4) Load Goals");
-            Console.WriteLine("5) Record Event");
-            Console.WriteLine("6) Quit");
-            Console.WriteLine("");
-            Console.WriteLine("Select a choice for the menu:");
-            string choice = Console.ReadLine();
-
-            if(choice == "1")
-            {
-                Console.WriteLine("The types of Goals are:"); 
-                Console.WriteLine("1. Simple Goal"); 
-                Console.WriteLine("2. Eternal Goal");
-                Console.WriteLine("3. Checklist Goal");
-                Console.Write("Which type of goal would you like to create?");
-                string input = Console.ReadLine();
-                if(input == "1")
-                {
-                    EnterGoalDetails();
-                }
-                else if(input == "2")
-                {
-                    EnterGoalDetails();
-                }
-                else if (input == "3")
-                {
-                    EnterGoalDetails();
-                }
-            }
-            else if(choice == "2")
-            {
-                foreach (string[] goalInputs in goalList)
-                {
-                    foreach (string item in goalInputs)
-                        {
-                            Console.Write(item);
-                        }
-                }
-            }
-            else if(choice == "3")
-            {
-                GoalManager();
-
-            }
-            else if(choice == "4"){
-                
-            }
-            else if(choice == "5"){
-                
-            }
-            else if(choice == "6")
-            {
-                Console.WriteLine("Goobye");
-                runProgram = true;
-
-            }
-        }
+        return _type;
     }
-    public void EnterGoalDetails()
+    public string GetName()
     {
-        string[] goalInputs = new string[3];
-        Console.WriteLine("What is the name of your goal?");
-        goalInputs[0] = Console.ReadLine();
-        Console.WriteLine("What is a short description of it?");
-        goalInputs[1] = Console.ReadLine();
-        Console.WriteLine("What is the amount of points associated with this goal?");
-        goalInputs[2] = Console.ReadLine();
-
-        goalList.Add(goalInputs);
-
-     
+        return _name;
     }
-    public void GoalManager()
+    public string GetDescription()
     {
-        string fileName = "";
-        Console.WriteLine("What is the filename for the goal file?");
-        fileName = Console.ReadLine();
-        using(StreamWriter writer = new StreamWriter(fileName))
-        {
-            foreach(string[] goalInputs in goalList)
-            {
-                writer.Write(goalInputs[0]+","+goalInputs[1]+","+ goalInputs[2]);
-            }
-
-        }
-
+        return _description;
     }
- 
+    public int GetPoints()
+    {
+        return _points;
+    }
+
+
+    // Methods
+    public abstract void ListGoal(int i);
+    public abstract string SaveGoal();
+    public abstract string LoadGoal();
+    public abstract void RecordGoalEvent(List<Goal> goals);
+
 }
