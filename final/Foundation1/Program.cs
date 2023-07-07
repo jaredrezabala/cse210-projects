cde5663ca8b81e8ab60906699ab15313d3ac9e18
jaredrezabala <rezabalajared@gmail.com>
2023-07-07 18:16:37 -0500

    More videos added

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -7,7 +7,7 @@ class Program
     {
         List<Video> videos = new List<Video>();
 
-        // Create Video objects and set their attributes
+        // Create Video 1
         Video video1 = new Video
         {
             Title = "Video 1",
@@ -19,11 +19,43 @@ class Program
         // Add comments to Video 1
         video1.AddComment("User 1", "Great video!");
         video1.AddComment("User 2", "I learned a lot.");
+        video1.AddComment("User 3", "Can you make more videos like this?");
+        video1.AddComment("User 4", "This video helped me solve my problem.");
 
         videos.Add(video1);
 
-        // Repeat the process for other videos
-        // ...
+        // Create Video 2
+        Video video2 = new Video
+        {
+            Title = "Video 2",
+            Author = "Author 2",
+            Length = 180,
+            Comments = new List<Comment>()
+        };
+
+        // Add comments to Video 2
+        video2.AddComment("User 1", "Nice explanation!");
+        video2.AddComment("User 2", "I have a question regarding this topic.");
+        video2.AddComment("User 3", "Please provide more examples.");
+        video2.AddComment("User 4", "Your videos are always helpful.");
+
+        videos.Add(video2);
+
+        // Create Video 3
+        Video video3 = new Video
+        {
+            Title = "Video 3",
+            Author = "Author 3",
+            Length = 150,
+            Comments = new List<Comment>()
+        };
+
+        // Add comments to Video 3
+        video3.AddComment("User 1", "I didn't understand the last part.");
+        video3.AddComment("User 2", "Could you explain it in more detail?");
+        video3.AddComment("User 3", "Great job, keep it up!");
+
+        videos.Add(video3);
 
         // Iterate through the list of videos
         foreach (Video video in videos)
@@ -44,4 +76,5 @@ class Program
             Console.WriteLine();
         }
     }
+
 }
