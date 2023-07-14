5ba6d1520da89bcba9a9eb8d7f4849a2fe7e9441
jaredrezabala <rezabalajared@gmail.com>
2023-07-13 19:42:24 -0500

    final 2

--- final/Foundation4/Activity.cs
+++ final/Foundation4/Activity.cs
@@ -1,12 +1,18 @@
 public class Activity
 {
     private DateTime _date;
-    protected int _length;
+    protected double _length;
 
     public DateTime Date
     {
         get { return _date; }
         set { _date = value; }
+    }
+
+    public double Length
+    {
+        get { return _length; }
+        set { _length = value; }
     }
 
     public virtual double GetDistance()
--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -6,21 +6,21 @@ class Program
 
         // Create Running Activity
         Running runningActivity = new Running();
-        runningActivity.Distance = 5.5; // Set distance in miles
         runningActivity.Length = 30; // Set length in minutes
+        runningActivity.Distance = 5.5; // Set distance in miles
         activities.Add(runningActivity);
 
         // Create Swimming Activity
         Swimming swimmingActivity = new Swimming();
-        swimmingActivity.Distance = 1.2; // Set distance in miles
         swimmingActivity.Length = 45; // Set length in minutes
+        swimmingActivity.Distance = 1.2; // Set distance in miles
         swimmingActivity.Laps = 20; // Set number of laps
         activities.Add(swimmingActivity);
 
         // Create Cycling Activity
         Cycling cyclingActivity = new Cycling();
-        cyclingActivity.Distance = 10.0; // Set distance in miles
         cyclingActivity.Length = 60; // Set length in minutes
+        cyclingActivity.Distance = 10.0; // Set distance in miles
         cyclingActivity.Speed = 18.5; // Set speed in miles per hour
         activities.Add(cyclingActivity);
 
